d431b28cccef61be44df498462a490c3b0efe6d6
Sophia Bell <sophbell87@gmail.com>
2020-06-25 13:24:17 +0100

    picture and links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-// import picture from '../public/images/sbell.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="/images/sbell.png" className="picture" alt="picture" />
-        <p>
+        <div className="profile-img-container">
+          <img src="/images/sbell.png" className="picture" alt="picture" />
+        </div>
+        <h1>
           Sophia Bell
-        </p>
+        </h1>
+        <nav>
+          <a href="https://github.com/Kittaru87" add target="_blank">Github</a>
+          <a href="https://www.linkedin.com/in/sophia-bell-1a945a2b/" add target="_blank">LinkedIn</a>
+          <a href="https://medium.com/@sophbell87" add target="_blank">Blog</a>
+          <a href="mailto:sophbell87@gmail.com" add target="_blank">Email</a>
+        </nav>
         {/* <a
           className="App-link"
           href="https://reactjs.org"
