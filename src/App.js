f261816b378e71526b9c31837a079bdc73db34ac
Sophia Bell <sophbell87@gmail.com>
2020-06-25 11:59:31 +0100

    adding picture and package-lock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+// import picture from '../public/images/sbell.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/images/sbell.png" className="picture" alt="picture" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sophia Bell
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
