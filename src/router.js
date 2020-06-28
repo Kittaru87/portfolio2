67f8be121d40957b4ec5286cd2d034ba16f63d86
Sophia Bell <sophbell87@gmail.com>
2020-06-28 22:34:54 +0100

    adding serve via npm

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Projects from "./components/Projects";
-import About from "./components/About";
-import Home from "./components/Home";
+// import React from "react";
+// import Projects from "./components/Projects";
+// import About from "./components/About";
+// import Home from "./components/Home";
 
-const routes = {
-  "/": () => <Home />,
+// const routes = {
+//   "/": () => <Home />,
   // "/about": () => <About />,
   // "/projects": () => <Projects />
-};
-export default routes;
+// };
+// export default routes;
