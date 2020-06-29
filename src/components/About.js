9669968ae755008c9473d8ccf1eeb26778595b84
Sophia Bell <sophbell87@gmail.com>
2020-06-29 11:15:06 +0100

    css and removing superfluous imports

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import { bounce } from 'react-animations';
-import '../App.css';
+import './About.css';
 
 function About() {  
   const bounceAnimation = keyframes`${bounce}`;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import Fade from 'react-reveal/Fade';
-import '../App.css';
+
 
 function Projects() {
   const animateList = [
@@ -10,29 +10,27 @@ function Projects() {
     <a href="https://github.com/Kittaru87/bank-tech-test" add target="_blank" rel="noopener noreferrer">Bank Tech Test</a>
   ];
   
-
   return (
 
-      <>
-    <div>
-      <blockquote>
-        A selection of current and finished projects
-      </blockquote>
-    </div>
-    <Fragment>
-      {animateList.map((item, key) => (
-        <div key={key}>
-          <Fade top>
-            <blockquote>
-              {item}
-            </blockquote>
-          </Fade>
-        </div>
-      ))}
-    </Fragment>
-     </>
+    <>
+      <div>
+        <blockquote>
+          A selection of current and finished projects
+        </blockquote>
+      </div>
+      <Fragment>
+        {animateList.map((item, key) => (
+          <div key={key}>
+            <Fade top>
+              <blockquote>
+                {item}
+              </blockquote>
+            </Fade>
+          </div>
+        ))}
+      </Fragment>
+    </>
 
- 
   )
 }
 
