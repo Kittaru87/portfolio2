6f55a6eeff8caa412e2882ea5b276aef9b3e0028
Sophia Bell <sophbell87@gmail.com>
2020-06-29 10:59:53 +0100

    adding bounce and fade in animations

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import { bounce } from 'react-animations';
 import '../App.css';
 
-function About() {
+function About() {  
+  const bounceAnimation = keyframes`${bounce}`;
+  const BouncyDiv = styled.div`
+                    animation: 2s ${bounceAnimation};
+                    `;
 
   return (
     <div>
     <blockquote>
-      Hello <span role="img" aria-label="wave">👋</span>
+      <BouncyDiv>Hello <span role="img" aria-label="wave">👋</span></BouncyDiv>
     </blockquote>
     </div>
  
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import Fade from 'react-reveal/Fade';
 import '../App.css';
 
 function Projects() {
+  const animateList = [
+    <a href="https://github.com/KG700/mix-tape" add target="_blank" rel="noopener noreferrer">Mix Tape</a>, 
+    <a href="https://github.com/Kittaru87/Innercise" add target="_blank" rel="noopener noreferrer">Innercise</a>, 
+    <a href="https://github.com/Kittaru87/acebook-theUndefinedMethod" add target="_blank" rel="noopener noreferrer">Acebook</a>, 
+    <a href="https://github.com/Kittaru87/bank-tech-test" add target="_blank" rel="noopener noreferrer">Bank Tech Test</a>
+  ];
+  
 
   return (
-    
+
+      <>
     <div>
-    <blockquote>
-      A selection of current and finished projects
-    </blockquote>
+      <blockquote>
+        A selection of current and finished projects
+      </blockquote>
     </div>
+    <Fragment>
+      {animateList.map((item, key) => (
+        <div key={key}>
+          <Fade top>
+            <blockquote>
+              {item}
+            </blockquote>
+          </Fade>
+        </div>
+      ))}
+    </Fragment>
+     </>
+
  
   )
 }
